d77d3f574bf948f52af1ee2cd8187f050b536592
Jashhvanth Tamilselvan Kunthavai <jashu.tamil@gmail.com>
2025-10-23 13:00:44 -0400

    updated some stuff
    i'm trying to understand this as I go along, that's why it's taking so long lol

--- movie_rec/data_manip.py
+++ movie_rec/data_manip.py
@@ -1,5 +1,15 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics.pairwise import cosine_similarity
+import scipy.sparse as sp
 
-df = pd.read_csv("movie_rec/data/TMDB_movie_dataset_v11.csv")
+def load():
+    df = pd.read_csv("movie_rec/data/TMDB_movie_dataset_v11.csv")
+    df = df[df['vote_count'] >= 10]
+    df = df[['id', 'title', 'genres', 'overview', 'vote_average', 'vote_count', 'runtime']].fillna('')
+    df['genres'] = df['genres'].apply(lambda x: x.split(", "))
+    return df
 
-print(df.head())
+def build_features():
+    
