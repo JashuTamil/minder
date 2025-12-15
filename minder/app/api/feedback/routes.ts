ff1bfcef4a1cff1257a6fd16b41cc0ca965dcfd4
Jashhvanth Tamilselvan Kunthavai <jashu.tamil@gmail.com>
2025-12-15 11:19:06 -0500

    finished writing the sending feedback code
    now I just need to test it and make sense of it

--- minder/app/api/feedback/routes.ts
+++ minder/app/api/feedback/routes.ts
@@ -18,3 +18,34 @@ export async function GET_USER_DATA() {
         return NextResponse.json({ error: 'Failed to fetch' }, { status: 500 })
     }
 }
+
+export async function SEND_USER_DATA(request: Request){
+    const clientData = await request.json()
+
+    try{
+        const response = await fetch('http://localhost:8000/api/v1/upload/send_feedback/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(clientData)
+        })
+
+        if (!response.ok){
+            throw new Error(`External API responded with status: ${response.status}`)
+        }
+
+        const responseData = await response.json()
+
+        return NextResponse.json({
+            status: 'success',
+            processedData: responseData,
+            message: 'Data successfully sent to backend.'
+        })
+    }
+
+    catch(error){
+        console.error("Error forwarding from backend:", error)
+        return NextResponse.json({ error: 'Failed to forward' }, { status: 500 })
+    }
+}
