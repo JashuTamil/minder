8b9371a71653c7b89949fdebfdd93d6cc08908e9
Jashhvanth Tamilselvan Kunthavai <jashu.tamil@gmail.com>
2026-03-23 11:19:41 -0400

    Made a few changes
    still need to test it and need to tweak the backend code, but it's a good start

--- minder/app/api/feedback/routes.ts
+++ minder/app/api/feedback/routes.ts
@@ -1,9 +1,22 @@
 import { NextResponse } from 'next/server';
+import { getAuth, User } from "firebase/auth";
 
 export async function GET_USER_DATA() {
     try{
+        const auth = getAuth()
+        const user: User | null = auth.currentUser
+        let uid: string = '';
+
+        if (user){
+            uid = user.uid;
+        }
+        else {
+            console.log("No user is currently signed in, or auth object is initializing.")
+        }
         
-        const response = await fetch('http://localhost:8000/api/v1/get/get_feedback')
+        const params = new URLSearchParams({userID: uid})
+
+        const response = await fetch(`http://localhost:8000/api/v1/get/get_feedback?${params}`)
 
         if (!response.ok) {
             throw new Error(`External API responded with status: ${response.status}`);
@@ -23,12 +36,26 @@ export async function SEND_USER_DATA(request: Request){
     const clientData = await request.json()
 
     try{
+
+        const auth = getAuth()
+        const user: User | null = auth.currentUser
+        let uid: string = '';
+
+        if (user){
+            uid = user.uid;
+        }
+        else {
+            console.log("No user is currently signed in, or auth object is initializing.")
+        }
+        
+        const params = new URLSearchParams({userID: uid})
+
         const response = await fetch('http://localhost:8000/api/v1/upload/send_feedback/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(clientData)
+            body: JSON.stringify({clientData, params})
         })
 
         if (!response.ok){
