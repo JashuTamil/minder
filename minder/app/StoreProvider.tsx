d0623ef7db1cf198e13b04cc581b25f817de8c36
Jashhvanth Tamilselvan Kunthavai <jashu.tamil@gmail.com>
2026-04-02 10:55:01 -0400

    finished up the implementation of after
    i gotta test it tomorrow

--- minder/app/StoreProvider.tsx
+++ minder/app/StoreProvider.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { Provider, useDispatch, useSelector, useStore } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import { fetchFeedback, fetchMovies, inputHandlingReducer } from './store/reducer/InputHandlingReducer'
+import { auth } from './lib/firebase'
+import { onAuthStateChanged } from 'firebase/auth';
+
 
 export const makeStore = () => {const store = configureStore({
    reducer: inputHandlingReducer,
 })
-  store.dispatch(fetchFeedback())
-  store.dispatch(fetchMovies())
-
   return store
 };
 
@@ -30,9 +30,37 @@ export default function StoreProvider({
   children: React.ReactNode
 }) {
   const storeRef = useRef<AppStore>(undefined)
+  const dispatchRef = useRef<AppDispatch | null>(null)
   if (!storeRef.current) {
     storeRef.current = makeStore()
+    dispatchRef.current = storeRef.current.dispatch
   }
+
+  useEffect(() => {
+    if (!dispatchRef.current) {
+      return
+    }
+
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log("User is logged in!")
+
+        try {
+          await dispatchRef.current!(fetchFeedback())
+          console.log("Fetch feedback is complete.")
+          await dispatchRef.current!(fetchMovies())
+          console.log("Fetch movies is complete")
+        } catch (error) {
+          console.error("Error:", error)
+        }
+      }
+      else {
+        console.log("User not logged in yet!")
+      }
+      return () => unsubscribe()
+    })
+
+  }, [])
 
   return <Provider store={storeRef.current}>{children}</Provider>
 }
